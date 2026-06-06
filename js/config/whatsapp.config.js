e11013e802dbc30f2556cfe20131d48eb1ef2386
Victor Muller <victormuller050@gmail.com>
2026-06-06 00:56:04 -0300

    atualizando

--- js/config/whatsapp.config.js
+++ js/config/whatsapp.config.js
@@ -3,4 +3,4 @@
  * Mensagem opcional ao abrir o WhatsApp.
  */
 window.WHATSAPP_NUMBER = "5541995919194";
-window.WHATSAPP_MESSAGE = "Olá! Vim pelo site da Victor Muller e quero saber mais sobre landing pages e orçamentos.";
+window.WHATSAPP_MESSAGE = "Olá! Vim pelo site da Convertix e quero saber mais sobre landing pages e orçamentos.";
